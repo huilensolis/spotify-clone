41b98c5ee20885a055e7f1fd72a9bd1975078b6f
Huilensolis <xywvanz@gmail.com>
2023-10-14 18:12:03 -0300

    femoved old dependency

--- src/models/nav-item/index.ts
+++ src/models/nav-item/index.ts
@@ -1,7 +1,5 @@
-import { TablerIconsProps } from '@tabler/icons-react'
-
 export interface NavItem {
-	icon: (props: TablerIconsProps) => JSX.Element
+	icon: (props: any) => JSX.Element
 	name: string
 	href: string
 	isActive: boolean
